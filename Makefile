340009e1e92460a6eae5410c3baeb36961570a29
Timo Kluck <tkluck@infty.nl>
2013-11-23 19:41:54 +0100

    Updates to the build scripts

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 all: pyperler
 
 pyperler: pyperler.pyx perl.pxd setup.py
-	LDFLAGS=`perl -MExtUtils::Embed -e ldopts` CFLAGS=`perl -MExtUtils::Embed -e ccopts` python setup.py build
+	python setup.py build
 	ln -sf `find build -name pyperler.so`
 interpret:
 	gcc interpret.c -o interpret `perl -MExtUtils::Embed -e ccopts -e ldopts`
 
 check: pyperler
 	python test.py
+
+clean:
+	rm -rf build pyperler.c
--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from subprocess import check_output
-cflags = check_output(["perl", "-MExtUtils::Embed", "-e", "ccopts"]).split()
-ldflags = check_output(["perl", "-MExtUtils::Embed", "-e", "ldopts"]).split()
+
+import os
+perl = os.environ.get('PERL', 'perl')
+
+cflags = check_output([perl, "-MExtUtils::Embed", "-e", "ccopts"]).split()
+ldflags = check_output([perl, "-MExtUtils::Embed", "-e", "ldopts"]).split()
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("pyperler", ["pyperler.pyx", "pyperler_extra.c"],
+    ext_modules = [Extension("pyperler", "perl.pxd pyperler.pyx pyperler_extra.c dlfcn.pxd".split(),
             extra_compile_args=cflags,
             extra_link_args=ldflags,
         )]
