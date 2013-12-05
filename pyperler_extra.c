a0fd141bd9a578d1206d76bf3c3be356bbc17117
Timo Kluck <tkluck@infty.nl>
2013-12-05 22:25:08 +0100

    Add licensing headers to source files

--- pyperler_extra.c
+++ pyperler_extra.c
@@ -1,3 +1,22 @@
+/*
+ * pyperler_extra.c
+ *
+ * Copyright (C) 2013, Timo Kluck <tkluck@infty.nl>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <EXTERN.h> /* from the Perl distribution */
 #include <perl.h> /* from the Perl distribution */
 PerlInterpreter *my_perl;
