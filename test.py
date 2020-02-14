b20dc215f453d9b1993e09847e9d78bf8afe077c
Timo Kluck <tkluck@infty.nl>
2020-02-14 10:27:57 +0100

    test: call testfile with keyword-style argument so it is self-documenting

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import os
 __dir__ = os.path.dirname(__file__)
 
 failure_count_1, test_count_1 = doctest.testmod(pyperler)
-failure_count_2, test_count_2 = doctest.testfile(os.path.join(__dir__,"README.md"), False)
+failure_count_2, test_count_2 = doctest.testfile(os.path.join(__dir__,"README.md"), module_relative=False)
 
 failure_count = failure_count_1 + failure_count_2
 sys.exit(
